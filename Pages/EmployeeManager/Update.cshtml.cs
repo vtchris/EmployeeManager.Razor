889df5fca304f64dc754cd839ce481e96fb3d357
vtchris <vtchris.2357@gmail.com>
2021-04-15 12:44:17 -0400

    43: Update employee

--- Pages/EmployeeManager/Update.cshtml.cs
+++ Pages/EmployeeManager/Update.cshtml.cs
@@ -2,15 +2,81 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EmployeeManager.Razor.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace EmployeeManager.Razor.Pages.EmployeeManager
 {
     public class UpdateModel : PageModel
     {
-        public void OnGet()
+        private readonly AppDbContext db = null;
+
+        [BindProperty]
+        public Employee Employee { get; set; }
+        public List<SelectListItem> Countries { get; set; }
+        public string Message { get; set; }
+        public bool DataFound { get; set; }
+
+        public UpdateModel(AppDbContext db)
         {
+            this.db = db;
+        }
+
+        public void OnGet(int id)
+        {
+            FillCountries();
+            Employee = db.Employees.Find(id);
+
+            if(Employee == null)
+            {
+                DataFound = false;
+                Message = "Employee ID not found.";
+            }
+            else
+            {
+                DataFound = true;
+                Message = "";
+            }
+        }
+
+        public void OnPost()
+        {
+            FillCountries();
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    db.Employees.Update(Employee);
+                    db.SaveChanges();
+                    Message = "Employee Updated";
+                }
+                catch (DbUpdateException ex1)
+                {
+                    Message = ex1.Message;
+                    if (ex1.InnerException != null)
+                    {
+                        Message += ": " + ex1.InnerException.Message;
+                    }
+                }
+                catch (Exception ex2)
+                {
+                    Message = ex2.Message;
+                }
+            }
+        }
+        public void FillCountries()
+        {
+            List<SelectListItem> listOfCountries = (from c in db.Employees
+                                                    select new SelectListItem()
+                                                    {
+                                                        Text = c.Country,
+                                                        Value = c.Country
+                                                    }).Distinct().ToList();
+
+            Countries = listOfCountries;
         }
     }
 }
